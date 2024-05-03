436171e92476c3048446c9de17785515784ba324
ojh <kfetuss@gmail.com>
2024-05-03 17:43:43 +0900

    paging 공통으로 변경

--- spring5.3/src/main/webapp/static/js/comm/siteComm.js
+++ spring5.3/src/main/webapp/static/js/comm/siteComm.js
@@ -2,33 +2,86 @@
  * 일반적인 공통 함수
  */
 
-const G_TOKEN_KEY = 'AccessKeyJwt';
-/**
- * LocalStorage 관리
- */
-function fnSetLocalStorage(key, value){
-	localStorage.setItem(key, value);
-}
+	const G_TOKEN_KEY = 'AccessKeyJwt';
+	/**
+	 * LocalStorage 관리
+	 */
+	function fnSetLocalStorage(key, value){
+		localStorage.setItem(key, value);
+	}
+	
+	function fnGetLocalStorage(key){
+		return localStorage.getItem(key);
+	}
+	
+	function fnDelLocalStorage(key){
+		 localStorage.removeItem(key);
+	}
+	
+	/**
+	 * SessionStorage 관리
+	 */
+	function fnSetSessionStorage(key, value){
+		sessionStorage.setItem(key, value);
+	}
+	
+	function fnGetSessionStorage(key){
+		return sessionStorage.getItem(key);
+	}
+	
+	function fnDelSessionStorage(key){
+		 sessionStorage.removeItem(key);
+	}
 
-function fnGetLocalStorage(key){
-	return localStorage.getItem(key);
-}
 
-function fnDelLocalStorage(key){
-	 localStorage.removeItem(key);
-}
-
-/**
- * SessionStorage 관리
- */
-function fnSetSessionStorage(key, value){
-	sessionStorage.setItem(key, value);
-}
-
-function fnGetSessionStorage(key){
-	return sessionStorage.getItem(key);
-}
-
-function fnDelSessionStorage(key){
-	 sessionStorage.removeItem(key);
-}
+	//nowPage가 시작. 최대 5개까지만 보여주기.
+	function makePaging(nowPage,totalCnt,pagePerCnt,divName) {
+//		$("#paging").empty();
+		$('#'+divName).empty();
+		
+		let showPagingCnt = 5;//페이징은 5개까지만
+		let maxPagingCnt = 0;//
+		
+		let totalPageCnt = Math.floor(totalCnt / pagePerCnt) ;
+		let remainder = totalCnt % pagePerCnt;
+		if ( remainder != 0) {
+			totalPageCnt += 1; 
+		}
+		if(nowPage > totalPageCnt) {
+			nowPage = totalPageCnt;
+		}
+		
+		if( (nowPage + showPagingCnt - 1) < totalPageCnt ) {
+			maxPagingCnt = nowPage + showPagingCnt -1;
+		} else {
+			maxPagingCnt = totalPageCnt;
+		}
+		
+		console.log(totalPageCnt);
+		
+		let pagingHtml =  '';
+		if( nowPage == 1 ) {
+			pagingHtml = pagingHtml + '';
+		} else if ( nowPage > 1) {
+			pagingHtml = pagingHtml + '<span><span onclick="baseSearch(1);"> 처음 </span></span>';
+			if ( nowPage >= 2) {
+				pagingHtml = pagingHtml + '<span onclick="baseSearch('+(nowPage-1)+');"><span> 이전 </span></span>';
+			}
+		}
+		
+		for(var i = nowPage ; i <= maxPagingCnt ; i++) {
+			if( i == nowPage) {
+				pagingHtml = pagingHtml + '<strong> '+nowPage+' </strong>';
+			} else {
+				pagingHtml = pagingHtml + '<a href="javascript:baseSearch('+i+');" > '+i+' </a>';
+			}
+		}
+		if ( maxPagingCnt < totalPageCnt) {
+			pagingHtml = pagingHtml + '<a href="javascript:baseSearch('+(nowPage+1)+');" ><span > 다음 </span></a>';
+		}
+		if ( nowPage < totalPageCnt) {
+			pagingHtml = pagingHtml + '<a href="javascript:baseSearch('+totalPageCnt+');" ><span > 마지막 </span></a>';
+		}
+		
+		$('#'+divName).append(pagingHtml);
+	}
